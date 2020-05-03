612ed83e3aa0c16fdebdfd5bd8c343bcd06927fc
Elbek <62874603+UzBestMaster@users.noreply.github.com>
2020-05-03 20:24:27 +0500

    Update soat.py

--- soat.py
+++ soat.py
@@ -12,7 +12,7 @@ while True:
     x = datetime.datetime.now(tz=ok)
     x = x.strftime("%H:%M")
     app.send(functions.account.UpdateProfile(
-    first_name=str(x),
+    first_name="Elbekjon ⏰"+str(x),
     about="O'zbekistonda soat: " +str(x)
     ))
     time.sleep(30)
