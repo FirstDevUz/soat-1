ede5432efe549a1d8d6016b5736d18e43ee4b35f
Elbek <62874603+UzBestMaster@users.noreply.github.com>
2020-05-04 00:59:53 +0500

    Update soat.py

--- soat.py
+++ soat.py
@@ -23,6 +23,6 @@ while True:
     hafta = days[hafta]
     app.send(functions.account.UpdateProfile(
     first_name="🇺🇿 #FirstDev ⏰ "+str(soat),
-    about="⏰ " +str(soat) + " | 📆 " + str(kun) + "-" + str(oy) + " | 📅 " + str(yil) + "-yil" + " | 🗓 " + str(hafta)
+    about="⏰ " +str(soat) + " | 📆 " + str(kun) + "-" + str(oy) + " | 📅 " + str(yil) + "-yil" + " | 🗓 " + str(hafta) + " | 📝 Pyrogram profile"
     ))
     time.sleep(20)
